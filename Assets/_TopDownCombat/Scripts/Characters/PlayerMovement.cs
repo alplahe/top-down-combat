f1adc1f8928b6da612e915706d8ca71fcfd70e33
alplahe <albertoplazaherraiz@gmail.com>
2021-08-24 22:42:45 +0200

    Character movement update

--- Assets/_TopDownCombat/Scripts/Characters/PlayerMovement.cs
+++ Assets/_TopDownCombat/Scripts/Characters/PlayerMovement.cs
@@ -18,24 +18,70 @@ namespace TopDownCombat.Characters
     [SerializeField] private NavMeshAgent agent;
     [SerializeField] private CharacterType characterType;
 
+    [SerializeField, Range(0, 10)] private float speed;
+    [SerializeField] private float angularSpeed;
+
+    private const float SPEED_MULTIPLIER = 0.01f;
+
+    private Vector3 inputMovement;
+
     private void Awake()
     {
       agent = GetComponent<NavMeshAgent>();
+    }
+
+    private void OnValidate()
+    {
+      agent.speed = speed;
+      agent.angularSpeed = angularSpeed;
     }
 
     private void Update()
     {
       if(characterType == CharacterType.NPC)
       {
-        agent.SetDestination(target.position);
+        DoNPCBehaviour();
         return;
       }
       else if(characterType == CharacterType.Player)
       {
-
+        DoPlayerBehaviour();
       }
+
+      ApplyMovement();
     }
 
+    #region Characters
+    private void DoNPCBehaviour()
+    {
+      gameObject.tag = characterType.ToString();
+      gameObject.name = characterType.ToString();
+
+      agent.isStopped = false;
+      agent.SetDestination(target.position);
+    }
+
+    private void DoPlayerBehaviour()
+    {
+      gameObject.tag = characterType.ToString();
+      gameObject.name = characterType.ToString();
+
+      agent.isStopped = true;
+    }
+    #endregion
+
+    #region Movement
+    public void ApplyMovement()
+    {
+      float xDirection = inputMovement.x;
+      float zDirection = inputMovement.y;
+
+      Vector3 moveDirection = new Vector3(xDirection, 0.0f, zDirection);
+      transform.position += moveDirection * speed * SPEED_MULTIPLIER;
+    }
+    #endregion
+
+    #region Input System
     public void OnAttack(InputAction.CallbackContext value)
     {
       if (value.started)
@@ -46,9 +92,11 @@ namespace TopDownCombat.Characters
 
     public void OnMovement(InputAction.CallbackContext value)
     {
-      Vector2 inputMovement = value.ReadValue<Vector2>();
-      Debug.Log("inputMovement: " + inputMovement);
+      Vector2 _inputMovement = value.ReadValue<Vector2>();
+      Debug.Log("inputMovement: " + _inputMovement);
+      inputMovement = _inputMovement;
     }
+
     public void OnReturnToInitialScreen(InputAction.CallbackContext value)
     {
       if (value.started)
@@ -57,5 +105,6 @@ namespace TopDownCombat.Characters
         Messenger.Broadcast(BroadcastName.Game.OnReturnToInitialScreen);
       }
     }
+    #endregion
   }
 }
